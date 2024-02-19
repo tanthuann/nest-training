152efcbbc88bc83b416844bcce2f1da008f43056
tanthuann <truongtanthuan99@gmail.com>
2024-02-19 10:56:30 +0700

    setup: first init jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './auth.constants';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' }
+    })
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  exports: [AuthService]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
 
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.usersService.fineOne(username);
@@ -12,11 +16,10 @@ export class AuthService {
       throw new UnauthorizedException();
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password, ...result } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
+    const payload = { sub: user.userId, username: user.username };
 
-    return result;
+    return {
+      access_token: await this.jwtService.signAsync(payload)
+    };
   }
 }
