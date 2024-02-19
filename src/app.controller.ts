d9ffa40d22936c43b8dd57b13f926a75e4b96d7a
tanthuann <truongtanthuan99@gmail.com>
2024-02-19 16:30:25 +0700

    feat: Public decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,15 @@
+// libs
 import { Controller, Get } from '@nestjs/common';
+// services
 import { AppService } from './app.service';
+// decorators
+import { Public } from './app.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -7,14 +7,26 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
 // constants
 import { jwtConstants } from './auth.constants';
+import { IS_PUBLIC_KEY } from 'src/app.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ]);
+
+    if (isPublic) return true;
+
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
