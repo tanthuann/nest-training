bbc853831b308f9bfdd5873a802780a94cc920ff
tanthuann <truongtanthuan99@gmail.com>
2024-02-20 13:54:38 +0700

    setup: docker pg, and connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
+// libs
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// controllers
 import { AppController } from './app.controller';
+// services
 import { AppService } from './app.service';
+// modules
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5437,
+      database: 'nest-user-service-db',
+      username: 'tanthuann',
+      password: 'tanthuann',
+      autoLoadEntities: true,
+      // Only enable this option if your application is in development,
+      // otherwise use TypeORM migrations to sync entity schemas:
+      // https://typeorm.io/#/migrations
+      synchronize: true
+    }),
+    AuthModule,
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
